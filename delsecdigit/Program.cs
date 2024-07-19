f075db0deb9ca9c9506f0434eb15857985a44372
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-19 10:54:36 +0600

    Функция DelSecondDigit удаляет вторую цифру в трехзначном числе

---
+++ delsecdigit/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+void Main()
+{
+    int number = ReadInt("Введите ТРЕХЗНАЧНОЕ число: ");
+    if (number > 99 && number < 1000)
+    {
+        System.Console.Write(DelSecondDigit(number));
+    }
+    else
+        System.Console.Write("Вы ввели некорректное число!");
+}
+
+int DelSecondDigit(int number)
+{  
+    int result = Convert.ToInt32(number / 100 + "" + number % 10);
+
+    return result;
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
+
+
+
+
+
