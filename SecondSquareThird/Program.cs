fc339f8cde2acf1e24a4e8cb8da5e921f0d0d7e3
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-19 11:15:42 +0600

    Функция SecondSquareThird принимает трехзначное число и возводит вторую его цифру в степень значения третьей цифры

---
+++ SecondSquareThird/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+
+void Main()
+{
+    int number = ReadLine("Введите ТРЕХЗНАЧНОЕ число: ");
+    if (number > 99 && number < 1000)
+        System.Console.Write(SecondSquareThird(number));
+    else
+    {
+        System.Console.Write("Вы ввели некорректное число!");
+    }
+}
+
+int SecondSquareThird(int number)
+{
+    int secondDigit = number / 10 % 10; // Узнали вторую цифру
+    int result = 1;
+    for (int power = number % 10; power > 0; power--)
+        result *= secondDigit;
+    
+    return result;
+}
+
+int ReadLine(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
