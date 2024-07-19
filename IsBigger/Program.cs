fefd786f57a13fdf2ae16b60dd8dd35f19154742
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-19 17:54:46 +0600

    Функция IsBigger берет на вход два числа и сравнивает их

---
+++ IsBigger/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, 
+// которая на вход принимает два числа и выводит, 
+// какое число большее, а какое меньшее.
+
+void Main()
+{
+    int firstNumber = ReadInt("Введите первое число: ");
+    int secondNumber = ReadInt("Введите второе число: ");
+
+    IsBigger(firstNumber, secondNumber);
+}
+
+void IsBigger(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+        System.Console.Write("Первое число БОЛЬШЕ второго!");
+    else if (firstNumber < secondNumber)
+        System.Console.Write("Первое число МЕНЬШЕ второго!");
+    else  
+        System.Console.Write("Числа равны!");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
