2b67f26ff4cebcbc155e01221161ca0d14341a02
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-19 13:15:38 +0600

    Функция IsMultiplicity принимает на вход два числа и проверяет является ли первое число кратно второму, если не кратно, то выводит остаток от деления

---
+++ IsMultiplicity/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число некратно
+// второму, то программа выводит остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+void Main()
+{
+    int firstNumber = ReadInt("Введите первое число: ");
+    int secondNumber = ReadInt("Введите второе число: ");
+
+    IsMultiplicity(firstNumber, secondNumber);
+}
+
+void IsMultiplicity(int firstNumber, int secondNumber)
+{
+    if (firstNumber % secondNumber == 0)
+    {
+        System.Console.WriteLine("да");
+    }
+    else
+    {
+        int result = firstNumber % secondNumber;
+        System.Console.WriteLine($"нет, {result}");
+    }
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
+
+
