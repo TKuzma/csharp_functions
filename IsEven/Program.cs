25807780d7f54685c9dda8960e6a1035788a855d
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-23 21:54:33 +0600

    Функция IsEven проверяет введеноре число на четность

---
+++ IsEven/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, 
+// которая на вход принимает число number и выводит, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+void Main()
+{
+    int number = ReadInt("Введите число: ");
+    IsEven(number);
+
+}
+
+void IsEven(int number)
+{
+    if (number % 2 == 0)
+        System.Console.Write($"Число {number} четное!");
+    else 
+        System.Console.Write($"Число {number} нечетное!");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
