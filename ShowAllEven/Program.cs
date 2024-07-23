28efd786b1335373f6286d0f38087e671622e6b8
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-23 22:02:34 +0600

    Функция ShowAllEven на вход принимает натуральное число, на выходе выводит все четные числа от 1 до заданного числа

---
+++ ShowAllEven/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, 
+// которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+
+void Main()
+{
+    int number = ReadInt("Введите натуральное число: ");
+    ShowAllEven(number);
+}
+
+void ShowAllEven(int number)
+{
+    for (int i = 1; i <= number; i++)   
+        if (i % 2 == 0)
+            System.Console.Write(i + " ");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
