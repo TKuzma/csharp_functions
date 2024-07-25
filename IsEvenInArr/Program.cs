756e69f394ae90d8f59cc9ebd61840b5400687c0
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-25 12:03:17 +0600

    Функция IsEvenInArr задает массив на 10 чисел и определяет количество четных чисел в массиве

---
+++ IsEvenInArr/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: 
+// Задайте массив на 10 целых чисел.
+// Напишите программу, которая определяет
+// количество чётных чисел в массиве.
+
+void Main()
+{
+    int[] array = GenerateArray();
+    PrintArray(array);
+    System.Console.WriteLine();
+    System.Console.Write(CountEven(array));
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    foreach(int e in array)
+    {
+        if (e % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write(string.Join(", ", array));
+}
+
+int[] GenerateArray()
+{
+    int[] array = new int[10];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rand.Next(1, 101);
+
+    return array;
+}
+
+Main();
