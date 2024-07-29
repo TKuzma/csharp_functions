d64157bbd85427915448cc54eac7970113b8a226
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-29 12:14:00 +0600

    Функция ReverseString принимает строку, состоящую из слов, разделенных пробелами и формирует новую строку с этими же словами, заданными в обратном порядке

---
+++ ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4*(не обязательная): 
+// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+void Main()
+{
+    System.Console.WriteLine("Введите строку: ");
+    string str = Console.ReadLine();
+    System.Console.WriteLine(str);
+
+    string newStr = ReverseString(str);
+    System.Console.Write(newStr);
+}
+
+string ReverseString(string str)
+{
+    string[] newString = str.Split(' ');
+    Array.Reverse(newString);
+    return string.Join(" ", newString);
+}
+
+
+Main();
